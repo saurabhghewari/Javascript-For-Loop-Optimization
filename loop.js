a007daae4ff062016f3af1572fa4d5e79cbe6969
tulshiram <tulshiram.patil92@gmail.com>
2015-10-25 13:10:59 +0530

    Array changed to nums from rand.

--- loop.js
+++ loop.js
@@ -1,41 +1,47 @@
 (function(){
-      var rand = [];
+      var nums = [];
       console.time("Main");
       for(var i = 0; i<1000;++i) {
-            rand.push(Math.floor((Math.random() * 34359738368) + 1));
+            nums.push(Math.floor((Math.random() * 34359738368) + 1));
       }
       // for(var i = 0; i<1000;i++) {
-      //       rand.push(Math.floor((Math.random() * 34359738368) + 1));
+      //       nums.push(Math.floor((Math.random() * 34359738368) + 1));
       // }
       console.timeEnd("Main");
 
       //simple for loop with Post-Increment
       console.time("FOR");
-      for(var j = 0; j < rand.length; j++) {
-            square(rand[i]);
+      for(var j = 0; j < nums.length; j++) {
+            square(nums[j]);
       }
       console.timeEnd("FOR");
 
       //simple for Loop with Pre-Increment.
       console.time("FOR-PRE");
-      for(var q = 0; q < rand.length; ++q) {
-            square(rand[j]);
+      for(var q = 0; q < nums.length; ++q) {
+            square(nums[q]);
       }
       console.timeEnd("FOR-PRE");
 
       //Optimized Loop
       console.time("FORRANDLENGTH");
-      for(var p = 0, k = rand.length; p < k; ++p) {
-            square(rand[p]);
+      for(var p = 0, k = nums.length; p < k; ++p) {
+            square(nums[p]);
       }
       console.timeEnd("FORRANDLENGTH");
 
       //simple for Reverse loop
       console.time("FOR");
-      for(var s = rand.length; s > 0; --s) {
-            square(rand[s]);
+      for(var s = nums.length; s > 0; --s) {
+            square(nums[s]);
       }
       console.timeEnd("FOR");
+
+      console.time("FOEEACH");
+      nums.forEach(function(num){
+            square(num);
+      });
+      console.timeEnd("FOEEACH");
 
 })();
 
